f2d1d5dd5721ebe77a0c96f3a120c65e5bb54e99
rdijkman <r.m.dijkman@tue.nl>
2018-01-29 18:00:57 +0100

    Refined running example.

---
+++ simulator_engine/test/nl/tue/queueing/sandbox/SandboxRunningExample.java
@@ -0,0 +1,17 @@
+package nl.tue.queueing.sandbox;
+
+import nl.tue.bpmn.concepts.BPMNModel;
+import nl.tue.bpmn.parser.BPMNParseException;
+import nl.tue.bpmn.parser.BPMNParser;
+import nl.tue.queueing.QueueingNetwork;
+
+public class SandboxRunningExample {
+
+	public static void main(String args[]) throws BPMNParseException {
+		BPMNParser parser = new BPMNParser();
+		parser.parse("./resources/tests/queueing/Running Example.bpmn");
+		BPMNModel model = parser.getParsedModel();
+		QueueingNetwork qn = new QueueingNetwork(model);
+	}
+	
+}
